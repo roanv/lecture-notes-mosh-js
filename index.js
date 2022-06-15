2a2abb39e6955a48122e2a1a679fdb90906a7a89
roanvonb <roanvonb@gmail.com>
2022-06-15 16:52:19 +1000

    object literal / factory / constructor

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-console.log('test');
+
+// object literal
+const literalCircle = {
+    radius: 1, // member -> property
+    draw: function(){ // member -> method
+        console.log('object literal circle'); 
+    }
+};
+
+literalCircle.draw();
+
+// factory
+function circleFactory(radius){
+    return {
+        radius, // same as radius:radius
+        draw: function(){ 
+            console.log('factory circle'); 
+        }
+    };
+}
+
+const factoryCircle = circleFactory(1);
+factoryCircle.draw();
+
+// constructor
+function ConstructorCircle(radius){
+    this.radius = radius; // "this" references the empty object {} created by new
+    this.draw = function(){
+        console.log('constructor circle')
+    }
+}
+
+const constructorCircle = new ConstructorCircle(1) // creates an empty object {} then points "this" to it
+constructorCircle.draw();
+
+
